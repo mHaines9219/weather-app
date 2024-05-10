d7c3411a2e9b7a500db417ee2ff451dbd584bd23
mHaines331 <mhaines9219@gmail.com>
2024-05-10 10:05:31 -0700

    building API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 
 export default function Page() {
   const [temp, setTemp] = useState(0);
+  const [condition, setCondition] = useState('');
+  const [img, setImg] = useState('');
   async function getWeather() {
     const apiKey = process.env.NEXT_PUBLIC_API_KEY;
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=91601&aqi=no`;
@@ -11,6 +13,8 @@ export default function Page() {
       const weatherResults = await response.json();
       console.log(weatherResults);
       setTemp(weatherResults.current.temp_f);
+      setCondition(weatherResults.current.condition.text);
+      setImg(weatherResults.current.condition.icon);
     } catch (error) {
       console.log(error);
     }
@@ -21,6 +25,8 @@ export default function Page() {
       <h1>Weather</h1>
       <button onClick={getWeather}>Get Weather</button>
       <div>{temp}</div>
+      <div>{condition}</div>
+      <img src={img} />
     </>
   );
 }
